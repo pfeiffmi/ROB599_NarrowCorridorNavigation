ad00824363d09c54f8540186efb9b2a9ffb087a1
pfeiffmi <pfeiffmi@oregonstate.edu>
2025-05-30 19:36:04 -0700

    removed build log and log files

--- build/interface_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mike54pfeiffer/Desktop/School/ROB599/project_ws/install/interface_pkg'
--- install/robot_pkg/lib/python3.12/site-packages/robot_pkg/main.py
+++
@@ -1,5 +0,0 @@
-import rclpy
-import rclpy.logging
-
-def main():
-    rclpy.logging.get_logger("Test").info("Hello!")
