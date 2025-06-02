0b85bfae04fe73671215fbf3be18b0ffde45309c
pfeiffmi <pfeiffmi@oregonstate.edu>
2025-06-01 21:49:46 -0700

    added log statements and changed ROS parameters to object variables while debugging issue with action not starting; added cancel callback and callback_group to fix issue of not cancelling action

--- src/robot_pkg/robot_pkg/action_server_drive_until_wall.py
+++ src/robot_pkg/robot_pkg/action_server_drive_until_wall.py
@@ -4,16 +4,17 @@ from rclpy.action import ActionServer
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-from rclpy.action import CancelResponse
+from rclpy.action import CancelResponse, GoalResponse
 from robot_pkg_interfaces.action import DriveUntilWall
 
 from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
 
 class DriveUntilWallServer(Node):
     def __init__(self):
         super().__init__('drive_until_wall_server')
-        self.declare_parameter('default_stop_distance', 1.0) # default vals
-        self.declare_parameter('default_forward_speed', 0.5)
+        self.stop_distance = 1.0
+        self.forward_speed = 0.5
 
         # publishr for /cmd_vel -> gazebo bridge
         self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)
@@ -31,10 +32,12 @@ class DriveUntilWallServer(Node):
             DriveUntilWall,
             'drive_until_wall',
             self.execute_callback,
-            cancel_callback=self.cancel_callback
+            cancel_callback=self.cancel_callback,
+            callback_group = ReentrantCallbackGroup()
         )
 
         self.get_logger().info("DriveUntilWall action server started, ready for movement goals.")
+
 
     def lidar_callback(self, msg: LaserScan):
         # storing the scan so execute_callback can grab it
@@ -48,40 +51,58 @@ class DriveUntilWallServer(Node):
             return
 
         self.get_logger().info(f"[lidar] distance ahead = {distance_ahead:.3f} m")
-    
-    def cancel_callback(self, goal_handle):
+
+
+    def publish_velocity(self):
         twist = Twist()
-        twist.linear.x = 0.0
+        twist.linear.x = self.forward_speed
+        self.get_logger().info(f"publishing speed: {twist.linear.x}")
         twist.angular.z = 0.0
         self.cmd_pub.publish(twist)
-        
+
+
+    def destroy(self):
+        self._action_server.destroy()
+        super().destroy_node()
+
+    def goal_callback(self, goal_request):
+        """Accept or reject a client request to begin an action."""
+        # This server allows multiple goals in parallel
+        self.get_logger().info('Received goal request')
+        return GoalResponse.ACCEPT
+
+    def cancel_callback(self, goal_handle):
+        self.get_logger().info("Cancel callback executed")
+        self.forward_speed = 0.0
+        self.publish_velocity()
         return CancelResponse.ACCEPT
 
-    def execute_callback(self, goal_handle):
-        self.get_logger().info("Received goal: stop_distance = %.2f, forward_speed = %.2f" %
-                               (goal_handle.request.stop_distance, goal_handle.request.forward_speed))
+    async def execute_callback(self, goal_handle):
+        #self.get_logger().info("Received goal: stop_distance = %.2f, forward_speed = %.2f" %
+        #                       (goal_handle.request.stop_distance, goal_handle.request.forward_speed))
 
         feedback_msg = DriveUntilWall.Feedback()
         result_msg = DriveUntilWall.Result()
 
-        stop_distance = goal_handle.request.stop_distance
-        forward_speed = goal_handle.request.forward_speed
+        self.stop_distance = goal_handle.request.stop_distance
+        self.forward_speed = goal_handle.request.forward_speed
 
-        # NOTE: Couldn't get .accepted to work (come back to later?)
-        # goal_handle.accepted()
+        rate = self.create_rate(5)  # 10 Hz
 
-        rate = self.create_rate(10)  # 10 Hz
+        self.get_logger().info("Begin execution loop")
 
         try:
             while rclpy.ok():
+                self.get_logger().info(f"if-0: {goal_handle.is_cancel_requested}")
                 if not goal_handle.is_active:
-                    # If the goal was canceled externally, break
+                    self.get_logger().info(f"if-1: {not goal_handle.is_active}")
                     self.get_logger().info("DriveUntilWall goal canceled")
                     result_msg.success = False
                     goal_handle.canceled()
                     return result_msg
 
                 if self.latest_scan is None:
+                    self.get_logger().info(f"if-2: {self.latest_scan is None}")
                     # Haven't received any scan yet, so wait 1 cycle
                     rate.sleep()
                     continue
@@ -91,6 +112,7 @@ class DriveUntilWallServer(Node):
                 distance_ahead = self.latest_scan.ranges[center_idx]
 
                 if distance_ahead <= 0.0 or distance_ahead != distance_ahead:
+                    self.get_logger().info(f"if-3: {distance_ahead <= 0.0 or distance_ahead != distance_ahead}")
                     rate.sleep()
                     continue
 
@@ -99,30 +121,27 @@ class DriveUntilWallServer(Node):
                 goal_handle.publish_feedback(feedback_msg)
 
                 # Check if we need to stop:
-                if distance_ahead <= stop_distance:
-                    twist = Twist()
-                    twist.linear.x = 0.0
-                    twist.angular.z = 0.0
-                    self.cmd_pub.publish(twist)
+                if distance_ahead <= self.stop_distance:
+                    self.get_logger().info(f"if-4: {distance_ahead <= self.stop_distance}")
+                    self.forward_speed = 0.0
+                    self.publish_velocity()
                     # above stops the car!
 
                     self.get_logger().info("Goal reached: stopped at distance %.2f m" % distance_ahead)
 
-                    result_msg.success = True
-                    goal_handle.succeed()
-                    return result_msg
+                    #result_msg.success = True
+                    #goal_handle.succeed()
+                    #return result_msg
 
-                twist = Twist()
-                twist.linear.x = float(forward_speed)
-                twist.angular.z = 0.0
-                self.cmd_pub.publish(twist)
+                self.forward_speed = float(self.forward_speed)
+                self.publish_velocity()
 
                 rate.sleep()
         except:
-            goal_handle.canceled()
+            pass
 
-        goal_handle.canceled()
         result_msg.success = False
+        goal_handle.succeed()
         return result_msg
 
 
@@ -130,7 +149,7 @@ def main(args=None):
     rclpy.init(args=args)
     action_server = DriveUntilWallServer()
 
-    executor = MultiThreadedExecutor(num_threads=2) # multi-threading to avoid lidar sensor not updating
+    executor = MultiThreadedExecutor()
     executor.add_node(action_server)
     try:
         executor.spin()
